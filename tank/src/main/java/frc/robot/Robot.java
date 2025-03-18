10818e960b1212f32103bf66c9e9adba95d5f9e7
Landon <landon.fehrenbacher@evsck12.com>
2025-03-18 08:09:46 -0500

    initial

---
+++ tank/src/main/java/frc/robot/Robot.java
@@ -0,0 +1,104 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.util.sendable.SendableRegistry;
+import edu.wpi.first.wpilibj.CAN;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+/**
+ * This is a demo program showing the use of the DifferentialDrive class, specifically it contains
+ * the code necessary to operate a robot with tank drive.
+ */
+public class Robot extends TimedRobot {
+  private final DifferentialDrive m_robotDrive;
+ public final CommandXboxController driverXbox = new CommandXboxController(0);
+  
+
+  private final SparkMax leftLeader = new SparkMax(1, MotorType.kBrushed);
+  private final SparkMax rightLeader = new SparkMax(3, MotorType.kBrushed);
+  private final SparkMax rightFollower = new SparkMax(2, MotorType.kBrushed);
+  private final SparkMax leftFollower = new SparkMax(4, MotorType.kBrushed);
+  private final SparkMax roller = new SparkMax(5, MotorType.kBrushed);
+
+  /** Called once at the beginning of the robot program. */
+  public Robot() {
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+    m_robotDrive = new DifferentialDrive(leftLeader, rightLeader);
+
+
+ SparkMaxConfig globalConfig = new SparkMaxConfig();
+    SparkMaxConfig rightLeaderConfig = new SparkMaxConfig();
+    SparkMaxConfig leftFollowerConfig = new SparkMaxConfig();
+    SparkMaxConfig rightFollowerConfig = new SparkMaxConfig();
+
+    /*
+     * Set parameters that will apply to all SPARKs. We will also use this as
+     * the left leader config.
+     */
+
+
+    // Apply the global config and invert since it is on the opposite side
+
+    // Apply the global config and set the leader SPARK for follower mode
+    leftFollowerConfig
+        .apply(globalConfig)
+        .follow(leftLeader);
+
+    // Apply the global config and set the leader SPARK for follower mode
+    rightFollowerConfig
+        .apply(globalConfig)
+        .follow(rightLeader);
+
+    /*
+     * Apply the configuration to the SPARKs.
+     *
+     * kResetSafeParameters is used to get the SPARK MAX to a known state. This
+     * is useful in case the SPARK MAX is replaced.
+     *
+     * kPersistParameters is used to ensure the configuration is not lost when
+     * the SPARK MAX loses power. This is useful for power cycles that may occur
+     * mid-operation.
+     */
+    leftLeader.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    leftFollower.configure(leftFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    rightLeader.configure(rightLeaderConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    rightFollower.configure(rightFollowerConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    m_robotDrive.tankDrive(driverXbox.getLeftY(), -driverXbox.getRightY());
+    if (driverXbox.x().getAsBoolean()){
+      roller.set(.3);
+    }
+    else{
+      roller.set(0);
+    }
+  }
+}
+
+
